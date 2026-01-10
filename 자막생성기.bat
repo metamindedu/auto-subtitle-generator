72d10847f867abcf9d01cb75f2b1e993a26e9558
metamindedu <metamind@kakao.com>
2026-01-10 10:49:52 +0900

    자막생성기.bat 파일 수정 & 실행파일 바로가기 생성 및 시작메뉴 고정 방법 추가

--- 자막생성기.bat
+++ 자막생성기.bat
@@ -9,8 +9,7 @@ echo         자동 자막 생성기 실행 중...
 echo ===================================================
 echo.
 
-REM 가상환경 활성화 및 앱 실행
-call venv\Scripts\activate.bat
-python -m streamlit run app.py
+REM 가상환경의 Python을 직접 사용하여 앱 실행
+venv\Scripts\python.exe -m streamlit run app.py
 
 pause
